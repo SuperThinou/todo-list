6126772d5971c61cfc8f7aa12535c15b2f75c042
NathanBernard <nbx-off@outlook.fr>
2026-03-13 16:11:53 +0100

    Added new task button and form

--- src/dom.js
+++ src/dom.js
@@ -5,7 +5,11 @@ const thisWeekBtn = document.getElementById("thisWeekBtn");
 
 // main selectors
 const mainTitle = document.getElementById("mainTitle");
+const newTaskBtn = document.getElementById("newTaskBtn");
+const newTaskPopup = document.getElementById("newTaskPopup");
+const addBtn = document.getElementById("addBtn");
 
+// sidebar event listeners
 allTaskBtn.addEventListener("click", () => {
   mainTitle.textContent = "All Tasks";
 });
@@ -16,4 +20,13 @@ todayBtn.addEventListener("click", () => {
 
 thisWeekBtn.addEventListener("click", () => {
   mainTitle.textContent = "This week";
+});
+
+// main event listeners
+newTaskBtn.addEventListener("click", () => {
+  newTaskPopup.classList.remove("hidden");
+});
+
+addBtn.addEventListener("click", () => {
+  newTaskPopup.classList.add("hidden");
 });
